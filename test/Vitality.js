1dc3524a2cde433d8001afdc9e9b237447ab7b20
slava hatnuke <slava.hatnuke@gmail.com>
2013-12-17 23:22:10 +0200

    fixed spec

--- test/Vitality.js
+++ test/Vitality.js
@@ -3,7 +3,7 @@ require('should');
 var Vitality = require('../src/Vitality');
 
 describe('Vitality', function(){
-    describe('should be constructed', function(){
+    describe('can be constructed', function(){
         it('without arguments', function(){
             Vitality.new();
         })

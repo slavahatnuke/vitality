24187ec6fc5eb4c2641272146adf40502ac28645
slava hatnuke <slava.hatnuke@gmail.com>
2013-12-17 23:14:53 +0200

    fixed tests

--- test/Vitality.js
+++ test/Vitality.js
@@ -4,10 +4,8 @@ var Vitality = require('../src/Vitality');
 
 describe('Vitality', function(){
     describe('should be constructed', function(){
-
         it('without arguments', function(){
-            var vitality = new Vitality();
-            vitality.should.be.instanceOf(Vitality);
+            Vitality.new();
         })
     })
 })
